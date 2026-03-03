7a9709e7b306c97444a011e1385a8c499edfc988
123cobloc <61994791+123cobloc@users.noreply.github.com>
2026-03-03 16:28:09 +0100

    v1.1

--- uirework.user.js
+++ uirework.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         GeoGuessr Spectate UI Rework
 // @namespace    123cobloc
-// @version      1.0
+// @version      1.1
 // @description  Applies custom CSS on the spectate page
 // @author       123cobloc
 // @match        https://www.geoguessr.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geoguessr.com
 // @grant        none
-// @run-at       document-idle 
+// @run-at       document-idle
 // @supportURL   https://github.com/123cobloc/geoguessr-ui-rework/issues
 // @downloadURL  https://github.com/123cobloc/geoguessr-ui-rework/raw/main/uirework.user.js
 // @updateURL    https://github.com/123cobloc/geoguessr-ui-rework/raw/main/uirework.user.js
@@ -24,24 +24,24 @@
         div[class*="spectate-sidebar_root"] {
             display: none !important;
         }
-        
+
         div[class*="spectate-header_root"] {
             height: ${value}svh !important;
             width: ${value * 2.25}svw !important;
             margin: 0 auto !important;
         }
-        
+
         div[class*="spectate_layout_root"] {
             padding: calc(10svw / 6) !important;
         }
-        
+
         div[class*="settings_settingsButtonContainer"] {
             top: 2.5svh !important;
             right: 1.5svw !important;
             bottom: auto !important;
             left: auto !important;
         }
-        
+
         div[class*="spectate-map_buttonWrapper"] {
             bottom: calc(10svw / -6.3) !important;
         }
@@ -56,26 +56,34 @@
                 styleElement = document.createElement("style");
                 styleElement.id = styleId;
                 styleElement.textContent = newCss;
-                (document.head || document.documentElement).appendChild(styleElement);
+                document.head.appendChild(styleElement);
             }
-        } else if (styleElement) {
-            styleElement.remove();
+        } else {
+            if (styleElement) {
+                styleElement.remove();
+            }
         }
     }
 
-    // --- Gestione della navigazione SPA ---
-    
-    // Osservatore per i cambiamenti nell'URL (metodo più affidabile)
-    let lastUrl = location.href;
-    const observer = new MutationObserver(() => {
-        if (location.href !== lastUrl) {
-            lastUrl = location.href;
-            applyStyle();
-        }
-    });
+    const patchHistory = (type) => {
+        const original = history[type];
 
-    observer.observe(document, { subtree: true, childList: true });
+        return function (...args) {
+            const result = original.apply(this, args);
+            const event = new Event(type.toLowerCase());
+            window.dispatchEvent(event);
 
-    // Eseguiamo anche all'avvio
+            return result;
+        };
+    };
+
+    history.pushState = patchHistory('pushState');
+    history.replaceState = patchHistory('replaceState');
+
+    window.addEventListener('pushstate', applyStyle);
+    window.addEventListener('replacestate', applyStyle);
+    window.addEventListener('popstate', applyStyle);
+
     applyStyle();
+
 })();
